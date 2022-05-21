607377d504a89d5c89a38b566b67410a7a078653
Quelha <felipequelha@gmail.com>
2022-05-21 18:09:23 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Conversion
+{
+  public static void Main(string[] args)
+  {
+    int number, i;
+    int[] Binary = new int[100];
+    Console.Write("Digite um numero Decimal:");
+    number = int.Parse(Console.ReadLine());
+    //Console.Write(number);
+
+    for (i = 0; number > 0; i++)
+    {
+      Binary[i] = number % 2;
+      number /= 2;
+      //Console.Write(Binary[i]);
+      //Console.Write(number);
+    }
+
+    for (i = i - 1; i >= 0; i--)
+    {
+      Console.Write(Binary[i]);
+    }
+  }
+}
